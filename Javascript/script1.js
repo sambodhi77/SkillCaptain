ee52392765bcc8d129544c9fe2adda85db8cea15
sambodhi katarnavare <sambodhikatarnwre@gmail.com>
2026-01-21 20:56:49 +0530

    Javascript !

---
+++ Javascript/script1.js
@@ -0,0 +1,6 @@
+alert("Welcome to Tony’s Café — Where Genius Meets Coffee!");
+
+let name = prompt("What’s your name, dear guest?");
+let drink = prompt("What’s your favorite drink?");
+
+alert(`Hello, ${name}! Enjoy your ${drink} at Tony’s Café ☕`);
