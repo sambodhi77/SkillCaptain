037ee47f09474fa85a744b0c37fec1b5f6b26664
sambodhi katarnavare <sambodhikatarnwre@gmail.com>
2026-01-21 20:04:14 +0530

    Javascript

---
+++ Javascript/script.js
@@ -0,0 +1,14 @@
+
+console.log("Welcome to Tony’s Café — Where Genius Meets Coffee");
+
+let cafeName = "Tony’s Café";
+let owner = "Tony Stark";
+let location = "New York";
+let coffeePrice = 250;
+
+console.log("Cafe Name:", cafeName);
+console.log("Owner Name:", owner);
+console.log("Location:", location);
+console.log("Coffee Price: ₹" + coffeePrice);
+
+console.log(`Welcome to ${cafeName}! Run by ${owner}, located at ${location}.`);
